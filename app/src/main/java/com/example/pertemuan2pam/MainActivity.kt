2ae21c5d0c28a03357eab50f22a3a911d6646bbc
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-18 00:32:57 +0700

    untuk menampilkan UI pada layar

--- app/src/main/java/com/example/pertemuan2pam/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan2pam/MainActivity.kt
@@ -31,79 +31,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Pertemuan2PAMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   BasicColumn(modifier = Modifier.padding(innerPadding))
-
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun BasicColumn( modifier: Modifier = Modifier) {
-    Column (horizontalAlignment =  Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 50.dp)
-    ) {
-        Text("Login", fontSize = 50.sp, fontWeight = FontWeight.Bold )
-        Spacer(modifier = Modifier.padding(1.dp))
-        Text("Ini adalah halaman login",
-            fontSize = 30.sp,
-            fontStyle = FontStyle.Italic
-        )
-        Spacer(modifier = Modifier.padding(5.dp))
-        Image(
-            painter = painterResource(id = R.drawable.umy),
-            contentDescription = "",
-            modifier = Modifier.clip(CircleShape)
-        )
-        Text("Nama", fontSize = 35.sp, fontWeight = FontWeight.Bold )
-        Spacer(modifier = Modifier.padding(10.dp)
-
-        )
-
-        Text(
-            text = "Nur Hikmah",
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Blue // Ganti dengan warna yang Anda inginkan
-        )
-        Spacer(modifier = Modifier.padding(10.dp))
-        Text(
-            text = "20220140185",
-            fontSize = 30.sp,
-            fontStyle = FontStyle.Italic,
-
-        )
-        Spacer(modifier = Modifier.padding(1.dp))
-        Image(
-            painter = painterResource(id = R.drawable.foto),
-            contentDescription = "",
-            modifier = Modifier.clip(CircleShape)
-        )
-
-
 
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true) //
-@Composable
-fun GreetingPreview() {
-    Pertemuan2PAMTheme {
-        BasicColumn()
-    }
-}
